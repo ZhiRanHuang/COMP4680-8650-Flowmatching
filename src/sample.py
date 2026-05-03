c1c2a229612ce07b9af5b9480589fed02288b1e9
Zhiran Huang <u8140182@anu.edu.au>
2026-05-03 16:01:21 +1000

    v-prediction flow matching, model train completed

--- src/sample.py
+++ src/sample.py
@@ -3,33 +3,27 @@ import matplotlib.pyplot as plt
 from src.model import MLP
 import os
 
-# -----------------------
-# sampling function
-# -----------------------
 @torch.no_grad()
-def sample(model, n=1000, steps=50):
+def sample(model, n=1000, dim=2, steps=50):
     device = next(model.parameters()).device
     model.eval()
 
-    z = torch.randn(n, 2, device=device)
+    z = torch.randn(n, dim, device=device)
 
     ts = torch.linspace(1.0, 0.0, steps + 1, device=device)
 
     for i in range(steps):
-        t = ts[i].repeat(n)
-        t = t.clamp(1e-5, 1.0)
+        t = torch.full((n,), ts[i].item(), device=device)  # ⭐关键正确写法
 
         dt = ts[i + 1] - ts[i]
 
         v = model(z, t)
+
         z = z + v * dt
 
     return z
 
 
-# -----------------------
-# main script
-# -----------------------
 if __name__ == "__main__":
     device = (
         "mps" if torch.backends.mps.is_available()
@@ -37,29 +31,13 @@ if __name__ == "__main__":
         else "cpu"
     )
 
-    ckpt_path = os.path.join(
+    ckpt_path = os.path.join("checkpoints", "model_swiss_roll_D2.pt")
 
-        "checkpoints",
-
-        "model_swiss_roll_D2.pt"
-
-    )
-
-    # -----------------------
-    # model
-    # -----------------------
     model = MLP(dim=2).to(device)
-
     model.load_state_dict(torch.load(ckpt_path, map_location=device))
 
-    # -----------------------
-    # sampling
-    # -----------------------
     samples = sample(model).cpu().numpy()
 
-    # -----------------------
-    # plot
-    # -----------------------
     plt.figure(figsize=(5, 5))
     plt.scatter(samples[:, 0], samples[:, 1], s=2)
     plt.axis("equal")
